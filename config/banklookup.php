0d211add8dcc482df2c7361f138281dd30c58e01
VNWA <nhatnguyen.dev.fullstack@gmail.com>
2026-04-16 23:10:15 +0700

    gasdg

--- config/banklookup.php
+++ config/banklookup.php
@@ -3,9 +3,9 @@
 return [
     'api_key' => env('BANK_LOOKUP_API_KEY', ''),
     'secret_key' => env('BANK_LOOKUP_API_SECRET_KEY', ''),
-    'base_url' => env('BANK_LOOKUP_BASE_URL', 'https://api.banklookup.net'),
-    'account_path' => env('BANK_LOOKUP_ACCOUNT_PATH', '/api/account-name'),
-    'qr_path' => env('BANK_LOOKUP_QR_PATH', '/api/qr-image'),
+    'base_url' => 'https://api.banklookup.net',
+    'account_path' => '',
+    'qr_path' => '/qr-image',
     'bank_list_path' => '/bank/list',
     'timeout' => 20,
 ];
